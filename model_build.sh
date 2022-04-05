6ca22c01cd063475d6f2ab7d6af0aaa49bca0256
Yousung Choi <yousung2002@snu.ac.kr>
2022-04-05 13:15:04 +0900

    Add files which used for object detection training.  - follow main.ipynb to get ready of training data.  - training takes so much time, so I used model_build.sh to training at background.    (if you train in jupyter, it sometimes terminates by itself so I cannot done my training job.)

---
+++ model_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+start=`date +%s`
+echo $start
+PIPELINE_CONFIG_PATH='/home/yousungchoi/projects/project_coordination/workspace/training_demo/models/my_ssd_mobilenet_v2_fpnlite_640640.config'
+#MODEL_DIR='/home/yousungchoi/projects/project_coordination/workspace/training_demo/models/1st_trial'
+#PIPELINE_CONFIG_PATH='/home/yousungchoi/projects/project_coordination/workspace/training_demo/models/my_ssd_mobilenet_v2_coco17_tpu-8.config'
+MODEL_DIR='/home/yousungchoi/projects/project_coordination/workspace/training_demo/models/3rd_trial_210323'
+NUM_TRAIN_STEPS=1000
+SAMPLE_1_OF_N_EVAL_EXAMPLES=1
+CUDA_VISIBLE_DEVICES=2 python /home/yousungchoi/projects/project_coordination/models/research/object_detection/model_main_tf2.py \
+	    --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
+            --model_dir=${MODEL_DIR} 
+#     --checkpoint_dir=${MODEL_DIR}  
+#     --num_train_steps=${NUM_TRAIN_STEPS}
+end=`date +%s`
+echo $end
+runtime=$((end-start))
+echo $runtime
+
+# Training session
